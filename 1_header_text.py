c48af25519d069651c2f82c16519bd93b58af714
Tim Vivian-Griffiths <tim.vivian.griffiths@gmail.com>
2018-12-10 19:42:40 +0000

    Creating the simple text header file

---
+++ 1_header_text.py
@@ -0,0 +1,15 @@
+import dash
+import dash_html_components as html
+
+app = dash.Dash()
+
+app.layout = html.Div(
+    [
+        html.H1(
+            "My First Dash App"
+        )
+    ]
+)
+
+if __name__ == '__main__':
+    app.run_server()
